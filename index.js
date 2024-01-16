38dfc886f3f8ca09d3753b3a78090b1b34f58dbb
Hammad740 <mohammadhammadansari91@gmail.com>
2024-01-16 16:13:54 +0530

    parsing data

---
+++ index.js
@@ -0,0 +1,22 @@
+import { parse } from 'csv-parse';
+import fs from 'fs';
+
+const results = [];
+
+fs.createReadStream('kepler_data.csv')
+  .pipe(
+    parse({
+      comment: '#',
+      columns: true,
+    })
+  )
+  .on('data', (data) => {
+    results.push(data);
+  })
+  .on('error', (err) => {
+    console.log(err);
+  })
+  .on('end', () => {
+    console.log(results);
+    console.log('done');
+  });
